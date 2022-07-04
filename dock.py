d35ce0ad0d5fbce3d5a6103b8095bfe2f9e9e414
Marie <49714157+devopsinthecloud@users.noreply.github.com>
2022-07-04 09:24:28 +0200

    Add files via upload

---
+++ dock.py
@@ -0,0 +1,7 @@
+import applescript
+from applescript import tell 
+
+command = "bin/dev"
+directories = open("dirs.txt", "r")
+for directory in directories:
+    tell.app('Terminal', f'do script "cd {directory}; {command}"')
